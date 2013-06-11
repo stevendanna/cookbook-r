4daf8e1c61afa96dae9c50ef8c165cff7b33c04f
Guilhem Lettron <guilhem@lettron.fr>
2013-06-11 15:13:50 +0200

    Change install behaviour

--- recipes/install_package.rb
+++ recipes/install_package.rb
@@ -19,36 +19,14 @@
 # limitations under the License.
 #
 
-# On Ubuntu or Debian we install via the
-# CRAN apt repository.
-include_recipe "apt"
-
-r_version = node['r']['version']
-
-if node['platform'] == 'debian'
-  distro_name = "#{node['lsb']['codename']}-cran/"
-  keyserver_url = "pgp.mit.edu"
-  key_id = "381BA480"
-else
-  distro_name = "#{node['lsb']['codename']}/"
-  keyserver_url = "keyserver.ubuntu.com"
-  key_id = "E084DAB9"
-end
-
-apt_repository "cran-apt-repo" do
-  uri "#{node['r']['cran_mirror']}/bin/linux/#{node['platform']}"
-  distribution distro_name
-  keyserver keyserver_url
-  key key_id
-  action :add
-end
-
 package 'r-base' do
-  version r_version
+  version node['r']['version'] if node['r']['version']
   action :install
 end
 
-package 'r-base-dev' do
-  version r_version
-  action :install
+if node['r']['install_dev']
+  package 'r-base-dev' do
+    version node['r']['version'] if node['r']['version']
+    action :install
+  end
 end
