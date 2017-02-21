9f2d13b3f87b968e641378e93f17eec4f9bed97c
sfromm-plm <sfromm@patientslikeme.com>
2017-02-21 17:08:23 -0500

    Update default.rb
    Prevent rinruby from echoing.

--- libraries/default.rb
+++ libraries/default.rb
@@ -1,8 +1,7 @@
-
 def r_package_installed?(package)
   require 'rinruby'
-  R.echo(enable: false)
-  R.eval 'packages = installed.packages()[,1]'
-  packages = R.pull 'packages'
+  r = RinRuby.new(:echo=>false)
+  r.eval 'packages = installed.packages()[,1]'
+  packages = r.pull 'packages'
   packages.include?(package)
 end
